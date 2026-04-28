9c1117ca1c3c29aa0b4b6674dc4db6966fc0e6ce
Pavlo F <safdw2@gmai.com>
2026-04-28 21:43:55 +0300

    Contact bot created

---
+++ contact-bot.py
@@ -0,0 +1,409 @@
+from collections import UserDict
+from datetime import datetime, date
+import pickle
+
+
+class Field:
+    def __init__(self, value):
+        self._value = None
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        self._value = new_value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, number):
+        symbols_to_remove = ["+", "(", ")", "-", " "]
+
+        for symbol in symbols_to_remove:
+            number = number.strip().replace(symbol, "")
+
+        if number.isdigit() and 10 <= len(number) <= 12:
+            self._value = number
+        else:
+            raise ValueError("Enter correct number.")
+
+
+class Birthday(Field):
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, birthday_date):
+        try:
+            datetime.strptime(birthday_date, "%d.%m.%Y")
+            self._value = birthday_date
+        except ValueError:
+            raise ValueError("Invalid date format. Use DD.MM.YYYY.")
+
+
+class Record:
+    def __init__(self, name, birthday=None):
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = Birthday(birthday) if birthday else None
+
+    def add_phone(self, number):
+        phone = Phone(number)
+        self.phones.append(phone)
+
+    def remove_phone(self, number):
+        phone = self.find_phone(number)
+        if phone:
+            self.phones.remove(phone)
+        else:
+            raise ValueError(f"Phone {number} not found.")
+
+    def edit_phone(self, old_number, new_number):
+        phone = self.find_phone(old_number)
+        if phone:
+            new_phone_obj = Phone(new_number)
+            self.phones.remove(phone)
+            self.phones.append(new_phone_obj)
+        else:
+            raise ValueError(f"Phone {old_number} not found in contacts.")
+
+    def find_phone(self, number):
+        for phone in self.phones:
+            if phone.value == number:
+                return phone
+        return None
+
+    def days_to_birthday(self):
+        if not self.birthday:
+            return "Birthday is not set."
+        today = date.today()
+        birthday_date = datetime.strptime(self.birthday.value, "%d.%m.%Y").date()
+        try:
+            this_year_birthday = birthday_date.replace(year=today.year)
+        except ValueError:
+            this_year_birthday = birthday_date.replace(year=today.year, month=3, day=1)
+
+        if this_year_birthday < today:
+            try:
+                next_birthday = birthday_date.replace(year=today.year + 1)
+            except ValueError:
+                next_birthday = birthday_date.replace(year=today.year + 1, month=3, day=1)
+        else:
+            next_birthday = this_year_birthday
+
+        return (next_birthday - today).days
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+            return f"Contact {name} deleted."
+        return f"Contact {name} not found."
+
+    def rename(self, old_name, new_name):
+        if old_name not in self.data:
+            raise ValueError(f"Contact {old_name} doesn't exist.")
+        if new_name in self.data:
+            raise ValueError(f"Contact {new_name} already exists. Please choose another name.")
+
+        record = self.data.pop(old_name)
+        record.name = Name(new_name)
+        self.data[new_name] = record
+
+    def find_owner_by_phone(self, phone_number):
+        for record in self.data.values():
+            if record.find_phone(phone_number):
+                return record.name.value
+        return None
+
+    def iterator(self, n):
+        records = list(self.data.values())
+        for i in range(0, len(records), n):
+            chunk = records[i : i + n]
+            yield chunk
+
+    def save_to_file(self, filename="contact_book.pkl"):
+        with open(filename, "wb") as fh:
+            pickle.dump(self.data, fh)
+
+    def read_from_file(self, filename="contact_book.pkl"):
+        try:
+            with open(filename, "rb") as fh:
+                self.data = pickle.load(fh)
+        except FileNotFoundError:
+            pass
+
+    def search_contacts(self, query):
+        result = []
+        for record in self.data.values():
+            if query in record.name.value:
+                result.append(record)
+                continue
+            for phone in record.phones:
+                if query in phone.value:
+                    result.append(record)
+                    break
+        return result
+
+
+def input_error(func):
+    def check_errors(*args):
+        try:
+            return func(*args)
+        except IndexError:
+            return "Error: Please provide all the information."
+        except ValueError as e:
+            return f"Error: {e}"
+    return check_errors
+
+
+class Operator:
+
+    def __init__(self):
+        self.contact_book = AddressBook()
+        self.contact_book.read_from_file()
+        self.COMMANDS = {
+            "hello": lambda _: "How can I help you?",
+            "add": self.add_contact,
+            "change": self.change_contact,
+            "rename": self.rename_contact,
+            "find": self.search,
+            "show all": self.show_all_numbers,
+            "delete": self.delete_contact,
+            "remove": self.remove_phone_number,
+            "birthday": self.show_birthday,
+            "info": self.show_info,
+            "good bye": lambda _: "Good Bye!",
+            "close": lambda _: "Good Bye!",
+            "exit": lambda _: "Good Bye!"}
+        self.error_messages = [
+            "Please provide a contact name.",
+            "Please provide all the info.",
+            "Please provide name, old phone number, and new phone number."
+        ]
+
+    def empty_contact_book(self):
+        if len(self.contact_book) == 0:
+            return "Error: Contact book is empty."
+
+    def main(self):
+        print("Welcome to the helper bot! Enter 'info' to show available commands.")
+        while True:
+            user_input = input(">>> ").strip()
+            if not user_input:
+                print("Error: Please enter a command.")
+                continue
+
+            matched_command = None
+            for key in self.COMMANDS.keys():
+                if user_input.lower().startswith(key):
+                    matched_command = key
+                    break
+
+            if matched_command:
+                method = self.COMMANDS[matched_command]
+
+                args_str = user_input[len(matched_command):].strip()
+                args = args_str.split() if args_str else []
+
+                result = method(args)
+                print(result)
+
+                if result == "Good Bye!":
+                    self.contact_book.save_to_file()
+                    break
+            else:
+                print("Error: Unknown command.")
+
+    def show_info(self, args):
+        return (
+            "Available commands:\n"
+            "-------------------\n"
+            "hello                           - Greet the bot\n"
+            "add [name] [phone]              - Add a new contact or phone\n"
+            "change [name] [old] [new_phone] - Change an existing phone number\n"
+            "rename [old_name] [new_name]    - Change a contact's name\n"
+            "find [query]                    - Find contacts by name or phone number matches\n"
+            "show all                        - Show all contacts\n"
+            "delete [name]                   - Delete a whole contact\n"
+            "remove [name] [phone]           - Remove a specific phone\n"
+            "birthday [name]                 - Show days to next birthday\n"
+            "info                            - Show this help message\n"
+            "good bye, close, exit           - Exit the program"
+        )
+
+    def check_correct(self, args, error_messages, num_args):
+        if len(args) < len(error_messages) and len(args) < num_args:
+            raise ValueError(error_messages[len(args)])
+
+
+    @input_error
+    def add_contact(self, args):
+        self.check_correct(args, self.error_messages, 2)
+
+        name, phone = args[0], args[1]
+
+        birthday = args[2] if len(args) > 2 else None
+
+        owner = self.contact_book.find_owner_by_phone(phone)
+        if owner and owner != name:
+            raise ValueError(f"Phone {phone} is already assigned to contact {owner}.")
+
+        record = self.contact_book.find(name)
+
+        if record:
+            if record.find_phone(phone):
+                raise ValueError(f"Contact {name} already has phone {phone}.")
+
+            record.add_phone(phone)
+
+            if birthday:
+                record.birthday = Birthday(birthday)
+                return f"Phone {phone} and birthday added to contact {name}."
+
+            return f"Phone {phone} added to contact {name}."
+        else:
+            new_record = Record(name, birthday=birthday)
+            new_record.add_phone(phone)
+            self.contact_book.add_record(new_record)
+            return f"Contact {name} added."
+
+    @input_error
+    def change_contact(self, args):
+        self.check_correct(args, self.error_messages, 1)
+
+        name = args[0]
+        record = self.contact_book.find(name)
+
+        if not record:
+            return f"Error: Contact {name} doesn't exist."
+
+        self.check_correct(args, self.error_messages, 3)
+
+        old_phone, new_phone = args[1], args[2]
+
+        owner = self.contact_book.find_owner_by_phone(new_phone)
+        if owner and owner != name:
+            raise ValueError(f"Phone {new_phone} is already assigned to contact {owner}.")
+
+        record.edit_phone(old_phone, new_phone)
+        return f"Contact {name} changed."
+
+    @input_error
+    def delete_contact(self, args):
+        self.check_correct(args, self.error_messages, 1)
+
+        name = args[0]
+
+        return self.contact_book.delete(name)
+
+    @input_error
+    def rename_contact(self, args):
+        self.check_correct(args, self.error_messages, 2)
+
+        old_name, new_name = args[0], args[1]
+
+        self.contact_book.rename(old_name, new_name)
+
+        return f"Contact {old_name} successfully renamed to {new_name}."
+
+    @input_error
+    def remove_phone_number(self, args):
+        self.check_correct(args, self.error_messages, 1)
+
+        name = args[0]
+        result = self.contact_book.find(name)
+
+        if not result:
+            return f"Error: Contact {name} doesn't exist."
+
+        self.check_correct(args, self.error_messages, 2)
+
+        phone = args[1]
+
+        result.remove_phone(phone)
+        return f"Phone {phone} removed from contact {name}."
+
+    @input_error
+    def search(self, args):
+        self.check_correct(args, self.error_messages, 1)
+
+        query = args[0]
+
+        records = self.contact_book.search_contacts(query)
+        if records:
+            result = []
+            for record in records:
+                result.append(str(record))
+            return "\n".join(result)
+        else:
+            return f"Error: Contact with {query} is not found."
+
+    @input_error
+    def show_birthday(self, args):
+        self.check_correct(args, self.error_messages, 1)
+
+        name = args[0]
+        record = self.contact_book.find(name)
+
+        if not record:
+            return f"Error: Contact {name} doesn't exist."
+
+        if not record.birthday:
+            return f"Contact {name} doesn't have a birthday set."
+
+        days = record.days_to_birthday()
+        return f"There are {days} days left until {name}'s birthday."
+
+    def show_all_numbers(self, args):
+
+        if not self.contact_book.data:
+            return "Contact book is empty."
+
+        n = 3
+        records_generator = self.contact_book.iterator(n)
+
+        for page_number, page_chunk in enumerate(records_generator, 1):
+            print(f"--- Page {page_number} ---")
+
+            for record in page_chunk:
+                print(str(record))
+
+            if len(page_chunk) < n:
+                break
+
+            user_input = input("Press Enter to see the next page or type 'q' to quit: ")
+            if user_input.lower() == "q":
+                break
+
+        return "End of contacts."
+
+
+operator = Operator()
+operator.main()
+
